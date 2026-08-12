92c4bbc914b4ac64f3c7c42f774e70ffb7934d35
Hiba Ibrahim Khanfar <hibaibraheem140@gmail.com>
2026-08-12 15:38:57 +0300

    add detail template with choice list and handle 404 errors with get_object_or_404

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 def index(request):
@@ -8,8 +10,8 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     return HttpResponse(template.render(context, request))
 def detail (request,question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
